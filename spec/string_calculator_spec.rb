ffb97d95d928c3e0ff4f39316db0ab6e100c58fe
Ram Nayan <ram.nayan@antiersolutions.com>
2025-05-04 21:55:04 +0530

    Test cases for new line between numbers and negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -29,5 +29,21 @@ RSpec.describe StringCalculator do
         expect(calculator.add('10,20,30,40,50')).to eq(150)
       end
     end
+
+     context 'when the input contains newlines as delimiters' do
+      it 'returns the sum of the numbers' do
+        expect(calculator.add("1\n2,3")).to eq(6)
+        expect(calculator.add("4\n5\n6")).to eq(15)
+        expect(calculator.add("7,8\n9")).to eq(24)
+      end
+    end
+
+    context 'when the input contains negative numbers' do
+      it 'raises an exception' do
+        expect { calculator.add('1,-2,3') }.to raise_error(ArgumentError, 'Negative numbers not allowed: -2')
+        expect { calculator.add('-1,-2,-3') }.to raise_error(ArgumentError, 'Negative numbers not allowed: -1, -2, -3')
+        expect { calculator.add('10,-20,30,-40,50') }.to raise_error(ArgumentError, 'Negative numbers not allowed: -20, -40')
+      end
+    end
   end
 end
