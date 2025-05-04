8e28740bd8cc23c4c9df60fd5c45973903467fe3
Ram Nayan <ram.nayan@antiersolutions.com>
2025-05-04 22:07:18 +0530

    Validate test case for negative numbers and new line between numbers(pass test cases)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,16 @@ class StringCalculator
   def add input_str
     return 0 if input_str.empty?
 
+    #Input string can accept new line between numbers
+    input_str = input_str.gsub(/-\n+/, "#{','}-").split("\n").join(',')
     numbers = input_str.split(',').map(&:to_i)
+    invalid_numbers numbers
+
     numbers.sum
+  end
+
+  def invalid_numbers(numbers)
+    negative_nums = numbers.select(&:negative?)
+    raise ArgumentError, "Negative numbers not allowed: #{negative_nums.join(', ')}" unless negative_nums.empty?
   end
 end
